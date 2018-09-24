90eab5137d380697db8d93c2c98d178bb6dc3cf6
cristian <thegoodcristian@gmail.com>
2018-09-24 11:01:06 -0400

    Final commit for deliverable two

--- dateDifferenceLab.java
+++ dateDifferenceLab.java
@@ -9,17 +9,6 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class dateDifferenceLab {
-
-	/*
-	 * Here I introduce the main() method. This triggers an instance of the JVM
-	 * and begins the execution of the method dateDifferenceCalculator().
-	 */
-	
-	public static void main (String [] args) {
-		
-		dateDifferenceCalculator();
-		
-	}
 	
 	/*
 	 * I have written the task of my program in a separate method from the
@@ -132,5 +121,15 @@ public class dateDifferenceLab {
 		
 	}
 	
+	/*
+	 * Here I introduce the main() method. This triggers an instance of the JVM
+	 * and begins the execution of the method dateDifferenceCalculator().
+	 */
+	
+	public static void main (String [] args) {
+		
+		dateDifferenceCalculator();
+		
+	}	
 
 }
